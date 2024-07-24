da7310251b23230da02a8048db093bb9666e1881
nicolasbergami <62970206+nicolasbergami@users.noreply.github.com>
2024-07-24 12:42:44 -0300

    load balancer y mensajes

---
+++ messages/model/message.go
@@ -0,0 +1,16 @@
+package model
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Message struct {
+	Id         primitive.ObjectID `bson:"_id"`
+	UserId     int                `bson:"userid"`
+	PropertyId string             `bson:"propertyid"`
+	UserName   string             `bson:"username"`
+	Body       string             `bson:"body"`
+	CreatedAt  string             `bson:"createdat"`
+}
+
+type Messages []Message
